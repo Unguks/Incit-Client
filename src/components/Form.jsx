21e72ea995e2dc2e578b76608c24e4d16f10d70a
Attar Fikri <priquetrum@gmail.com>
2024-08-11 19:54:30 +0700

    making login/signup form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Facebook from '../icons/Facebook';
 
 export default function Form(){
     return (
@@ -50,18 +51,7 @@ export default function Form(){
                     </button>
 
                     <button className='flex rounder-xl py-2 border-2 border-gray-100 items-center justify-center gap-2 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out'>
-                    <svg className='max-h-5' viewBox="0 0 48 48">
-                        <title>Google Logo</title>
-                        <clipPath id="g">
-                            <path d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"/>
-                        </clipPath>
-                        <g class="colors" clip-path="url(#g)">
-                            <path fill="#FBBC05" d="M0 37V11l17 13z"/>
-                            <path fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z"/>
-                            <path fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z"/>
-                            <path fill="#4285F4" d="M48 48L17 24l-4-3 35-10z"/>
-                        </g>
-                        </svg>
+                    <Facebook/>
                         Sign in with Google
                     </button>
 
